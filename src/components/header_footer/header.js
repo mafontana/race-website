4b6f13e00ee366fdcd150fc13a67d0ee65cbd01d
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-10 10:20:50 -0600

    imported material ui header into header component

--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/core/Menu'
+import IconButton from '@material-ui/core/IconButton'
 
-class header extends Component {
+class Header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <AppBar
+                position="fixed"
+                style={{
+                    backgroundColor: '#2f2f2f',
+                    boxShadow: 'none',
+                    padding: '10px 0px'
+                }}
+            >
+            header
+            
+            </AppBar>
         );
     }
 }
 
-export default header;
+export default Header;
