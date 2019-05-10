0ca7b58c408eba214732839e3738c85791db31dc
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-10 17:02:32 -0600

    added background to register now button when hovered

--- src/components/featured/Countdown.js
+++ src/components/featured/Countdown.js
@@ -4,7 +4,49 @@ class Countdown extends Component {
     render() {
         return (
             <div className="countdown_wrapper">
-                
+                <div className="countdown_top">
+                    Race starts in
+                </div>
+                <div className="countdown_bottom">
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            23
+                        </div>
+                        <div className="countdown_tag">
+                            Days
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                    <div className="countdown_time">
+                        2
+                    </div>
+                    <div className="countdown_tag">
+                        Hours
+                    </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            10
+                        </div>
+                        <div className="countdown_tag">
+                            Minutes
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            45
+                        </div>
+                        <div className="countdown_tag">
+                            Seconds
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
             </div>
         );
     }
