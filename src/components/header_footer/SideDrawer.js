8056f86af04bc25403d65f2c48f7140fa165641f
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-10 10:50:16 -0600

    created side drawer component

---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer'
+
+
+const SideDrawer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SideDrawer;
