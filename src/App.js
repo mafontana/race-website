1c7f4b46b0eec4790cc71d165488d3b285f0962e
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-13 16:09:23 -0600

    created venueInfo component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './resources/styles.css'
 import Header from './components/header_footer/Header'
 import Featured from './components/featured'
+import VenueInfo from './components/venueInfo'
 
 function App() {
   return (
     <div className="App">
     <Header />
     <Featured />
+    <VenueInfo />
     </div>
   );
 }
---
+++ src/components/venueInfo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import icon_calendar from '../../resources/images/icons/calendar.png'
+import icon_location from '../../resources/images/icons/location.png'
+
+
+const VenueInfo = () => {
+    return (
+        <div className="bck_black">
+            hello VenueInfo
+        </div>
+    );
+};
+
+export default VenueInfo;
