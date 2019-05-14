6eaac8b42ba6af14b8d0d296a45bc9e17cf465ca
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-14 15:22:09 -0600

    created discount component in highlights section

---
+++ src/components/highlights/Discount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade'
+
+
+const Discount = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Discount;
