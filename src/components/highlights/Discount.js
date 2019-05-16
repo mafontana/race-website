55996132a99aa8e59fad5a92c98f0628b33ccb43
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-15 19:49:54 -0600

    cleaned up code in discount component

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -6,31 +6,20 @@ const Discount = () => {
     return (
         <div className="center_wrapper">
             <div className="discount_wrapper">
-                
-            
-            
-            <Fade>
-                <div className="discount_porcentage">
-                    <span>30%</span>
-                    <span>OFF</span>
-                </div>
-            </Fade>
-
-
-
-            <Slide right>
-            <div className="discount_description">
-                <h3>Buy race entry before August 1 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            
-                <div>button</div>
-            </div>
-            </Slide>
-
-
-
-            </div>
-            
+                <Fade>
+                    <div className="discount_porcentage">
+                        <span>30%</span>
+                        <span>OFF</span>
+                    </div>
+                </Fade>
+                <Slide right>
+                    <div className="discount_description">
+                        <h3>Buy race entry before August 1 </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        <div>button</div>
+                    </div>
+                </Slide>
+            </div> 
         </div>
     );
 };
