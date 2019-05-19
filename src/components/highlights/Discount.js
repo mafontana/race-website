009c33b2198d87e70f9529f084e702aa4125fb4b
Meredith Fontana <meredith.fontana@gmail.com>
2019-05-19 16:35:53 -0600

    created percentage function in discount component

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -7,16 +7,28 @@ class Discount extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+           discountStart: 0,
+           discountEnd: 30
         }
       }
     
+    percentage = () => {
+        if(this.state.discountStart < this.state.discountEnd ){
+            this.setState({
+                discountStart: this.state.discountStart + 1
+            })
+        }
+
+
+    }
     
     render() {
         return (
             <div className="center_wrapper">
             <div className="discount_wrapper">
-                <Fade>
+                <Fade
+                    onReveal={()=>this.percentage}
+                >
                     <div className="discount_porcentage">
                         <span>30%</span>
                         <span>OFF</span>
